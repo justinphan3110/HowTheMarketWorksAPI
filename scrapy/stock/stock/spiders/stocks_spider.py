fae7cc076afe5a43de6c6aa48ccd4bbbd45aae37
justinphan3110 <justinphan3110@gmail.com>
2019-07-11 09:26:42 +0700

    influxdb name: stock

--- scrapy/stock/stock/spiders/stocks_spider.py
+++ scrapy/stock/stock/spiders/stocks_spider.py
@@ -8,7 +8,6 @@ class StockSpider(scrapy.Spider):
   name = "stock"
   start_urls = ['https://finance.yahoo.com/quote/']
   db = InfluxDBClient ("localhost", 8086);
-  
   def parse(self, response):
       for company in self.companyList:
           link = self.start_urls[0] + company + '/'
@@ -16,7 +15,7 @@ class StockSpider(scrapy.Spider):
           yield scrapy.Request(link, callback = self.crawStock, meta = {'companyName' : company})
   def crawStock(self, response): 
       companyName = response.meta.get('companyName')
-      self.db.switch_database("test")
+      self.db.switch_database("stock")
       price = response.xpath('//div[@id="quote-header-info"]/div[@class="My(6px) Pos(r) smartphone_Mt(6px)"]/div//span/text()').extract()[0]
       growth = response.xpath('//div[@id="quote-header-info"]/div[@class="My(6px) Pos(r) smartphone_Mt(6px)"]/div//span/text()').extract()[1]
       json = [{
